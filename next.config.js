ae37dcb73d643cd2cea96608a6a4ad200a96f1ad
Rogin Farrer <rogin@roginfarrer.com>
2022-06-02 22:25:40 -0400

    remove styled-components compiler

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 module.exports = withVanillaExtract(nextConfig);
